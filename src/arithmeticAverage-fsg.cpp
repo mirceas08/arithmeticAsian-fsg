cc4eef88ec91b097ba65bb4029b02b514f3f778a
mirceas08 <mircea.simionica@gmail.com>
2015-11-19 00:15:54 +0100

    function now reads from file

--- src/arithmeticAverage-fsg.cpp
+++ src/arithmeticAverage-fsg.cpp
@@ -9,17 +9,8 @@ using namespace arma;
 
 int main(int argc, char** argv)
 {
-    int i, j;
+    std::string dataFile = "data/data.dat";
 
-    std::string optionStyle = "A";
-    int n = 60;
-    int numAverages = 100;
-    double S0 = 50.0;
-    double sigma = 0.40;
-    double r = 0.10;
-    double T = 1.0;
-    double K = 50.0;
-
-    cout << "Option price: " << forwardShootingGrid(n, S0, K, r, sigma, T, numAverages, optionStyle);
+    cout << "Option price: " << forwardShootingGrid(dataFile.c_str());
     return 0;
 }
--- src/forward_shooting_grid.h
+++ src/forward_shooting_grid.h
@@ -1,14 +1,65 @@
 #include <math.h>
+#include <stdlib.h>
 #include <string>
+#include <fstream>
+#include <sstream>
 #include "helpers.h"
 
 #include <armadillo>
 using namespace arma;
 
-double forwardShootingGrid(int n, double S0, double K, double r, double sigma, double T, int numAverages, std::string optionStyle)
+double forwardShootingGrid(std::string dataFile)
 {
     int i, j;
 
+    /* ------------------------ Set starting values from file ------------------------ */
+    int n;
+    double S0;
+    double K;
+    double r;
+    double T;
+    double sigma;
+    int numAverages;
+    std::string putCall;
+    std::string optionStyle;
+
+    std::ifstream fIN(dataFile.c_str());
+    std::string line;
+    getline(fIN, line); // skip header
+
+    while (std::getline(fIN, line)) {
+        std::stringstream stream(line);
+        std::string variable;
+        std::string value;
+
+        stream >> variable >> value;
+
+        if (variable == "nsteps")
+            n = atof(value.c_str());
+        else if (variable == "numAverages")
+            numAverages = atof(value.c_str());
+        else if (variable == "spot")
+            S0 = atof(value.c_str());
+        else if (variable == "strike")
+            K = atof(value.c_str());
+        else if (variable == "r")
+            r = atof(value.c_str());
+        else if (variable == "maturity")
+            T = atof(value.c_str());
+        else if (variable == "sigma")
+            sigma = atof(value.c_str());
+        else if (variable == "putCall")
+            putCall = value;
+        else if (variable == "optionStyle")
+            optionStyle = value;
+        else
+            break;
+    }
+
+    transform(putCall.begin(), putCall.end(), putCall.begin(), ::toupper);
+    transform(optionStyle.begin(), optionStyle.end(), optionStyle.begin(), ::toupper);
+
+    /* ------------------------ Initialize some values ------------------------ */
     vec strike(numAverages);
     strike.fill(K);
     vec zeroVec = zeros<vec>(numAverages);
@@ -23,14 +74,14 @@ double forwardShootingGrid(int n, double S0, double K, double r, double sigma, d
     field<vec> av(n+1, n+1);
     field<vec> optionPrice(n+1);
 
-    // Build the binomial tree for the stock
+    /* ------------------------ Build binomial tree for S ------------------------ */
 	for (j = 0; j <= n; j++) {
         for (i = 0; i <= j; i++) {
             S(i,j) = S0 * pow(u, j-i) * pow(d, i);
         }
 	}
 
-	// Shoot the averages
+	/* ------------------------ Shoot averages ------------------------ */
 	for (j = 0; j <= n; j++) {
         for (i = 0; i <= j; i++) {
             double first_max = (1- pow(u, j - i + 1)) / (1-u);
@@ -52,12 +103,12 @@ double forwardShootingGrid(int n, double S0, double K, double r, double sigma, d
         }
 	}
 
-	// Compute terminal payoffs
+	/* ------------------------ Compute terminal payoffs ------------------------ */
 	for (i = 0; i <= n; i++) {
         optionPrice(i) = max(av(i,n) - strike, zeroVec);
 	}
 
-	// Backward recursion
+	/* ------------------------ Backward recursion ------------------------ */
     for (j = n-1; j >= 0; j--) {
         for (i = 0; i <= j; i++) {
             vec currentS(numAverages);
@@ -72,14 +123,19 @@ double forwardShootingGrid(int n, double S0, double K, double r, double sigma, d
             if (optionStyle == "E") {
                 optionPrice(i) = std::exp(-r*dt) * ( p * interpOption_u + q * interpOption_d );
             }
-            else {
+            else if (optionStyle == "A") {
                 vec continuationValue = std::exp(-r*dt) * ( p * interpOption_u + q * interpOption_d );
                 vec exerciseValue = max(av(i,j) - strike, zeroVec);
                 optionPrice(i) = max(continuationValue, exerciseValue);
+            }
+            else {
+                cout << "optionStyle can be either E (european) or A (american)" << endl;
+                return -1;
             }
 
         }
     }
 
+    //return option price
     return optionPrice(0).at(0);
 }
