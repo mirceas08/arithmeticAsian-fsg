3c2e872b9e8e5b32731ac8c14f33388656aabad3
mirceas08 <mircea.simionica@gmail.com>
2015-11-23 18:49:24 +0100

    added new lattice strategies

--- src/binomialStrategy.cpp
+++ src/binomialStrategy.cpp
@@ -4,7 +4,6 @@
 #include "binomialStrategy.h"
 #include <cmath>
 
-
 /* ------------------------ Base class ------------------------ */
 
 BinomialStrategy::BinomialStrategy(double _sigma, double _r, double _dt)
@@ -42,5 +41,69 @@ JarrowRudd::JarrowRudd(double sigma, double r, double dt):
 }
 
 JarrowRudd::~JarrowRudd() {}
+
+
+/* ------------------------ Tian  ------------------------ */
+
+Tian::Tian(double sigma, double r, double dt):
+    BinomialStrategy(sigma, r, dt)
+{
+    double v = std::exp(sigma*sigma*dt);
+    double a = std::exp(r*dt);
+    double sqrtV = std::sqrt(v*v + 2*v - 3);
+
+    u = 0.5 * a * v * (v + 1 + sqrtV);
+    d = 0.5 * a * v * (v + 1 - sqrtV);
+    p = (a-d) / (u-d);
+}
+
+Tian::~Tian() {}
+
+/* ------------------------ Jarrow-Rudd Risk Neutral ------------------------ */
+
+JarrowRudd_RN::JarrowRudd_RN(double sigma, double r, double dt):
+    BinomialStrategy(sigma, r, dt)
+{
+    double drift = r - 0.5 * sigma * sigma;
+    u = std::exp(drift*dt + sigma * std::sqrt(dt));
+    d = std::exp(drift*dt - sigma * std::sqrt(dt));
+    p = (std::exp(r*dt) - d) / (u-d);
+}
+
+JarrowRudd_RN::~JarrowRudd_RN() {}
+
+
+/* ------------------------ Cox-Ross-Rubinstein with drift ------------------------ */
+
+CRR_drift::CRR_drift(double sigma, double r, double dt, double _eta):
+    BinomialStrategy(sigma, r, dt), eta(_eta)
+{
+    u = std::exp(eta*dt + sigma * std::sqrt(dt));
+    d = std::exp(eta*dt -sigma * std::sqrt(dt));
+    p = (std::exp(r*dt) - d) / (u-d);
+}
+
+CRR_drift::~CRR_drift() {}
+
+/* ------------------------ Leisen-Reimer ------------------------ */
+
+double h(double x, int n);
+
+LeisenReimer::LeisenReimer(double sigma, double r, double dt, double _S0, double _K, double _T, int _n):
+    BinomialStrategy(sigma, r, dt), S0(_S0), K(_K), T(_T), n(_n)
+{
+    double sqrtT = sigma * std::sqrt(T);
+    double d1 = (std::log(S0/K) + (r + 0.5 * sigma * sigma) * T) / sqrtT;
+    double d2 = d1 - sqrtT;
+
+    p_bar = h(d1, n);
+    p = h(d2, n);
+
+    u = std::exp(r*dt) * (p_bar / p);
+    d = (std::exp(r*dt) - p * u) / (1.0 - p);
+}
+
+LeisenReimer::~LeisenReimer() {}
+
 
 #endif // BINOMIALSTRATEGY_CPP
--- src/binomialStrategy.h
+++ src/binomialStrategy.h
@@ -13,11 +13,11 @@ public:
     double sigma;
     double dt;
 public:
-    BinomialStrategy(double sigma, double r, double dt);
+    BinomialStrategy(double _sigma, double _r, double _dt);
     virtual ~BinomialStrategy();
 };
 
-// Cox-Ross-Rubinstein
+// Cox-Ross-Rubinstein (CRR)
 class CRR: public BinomialStrategy
 {
 public:
@@ -25,12 +25,52 @@ public:
     virtual ~CRR();
 };
 
-// Jarrow-Rudd
+// Jarrow-Rudd (JR)
 class JarrowRudd: public BinomialStrategy
 {
 public:
     JarrowRudd(double sigma, double r, double dt);
     virtual ~JarrowRudd();
+};
+
+// Tian (TIAN)
+class Tian: public BinomialStrategy
+{
+public:
+    Tian(double sigma, double r, double dt);
+    virtual ~Tian();
+};
+
+// Jarrow-Rudd Risk Neutral (JR-RN)
+class JarrowRudd_RN: public BinomialStrategy
+{
+public:
+    JarrowRudd_RN(double sigma, double r, double dt);
+    virtual ~JarrowRudd_RN();
+};
+
+// Cox-Ross-Rubinstein with drift (CRR-drift)
+class CRR_drift: public BinomialStrategy
+{
+public:
+    double eta;
+public:
+    CRR_drift(double sigma, double r, double dt, double _eta);
+    virtual ~CRR_drift();
+};
+
+// Leisen-Reimer (LR)
+class LeisenReimer: public BinomialStrategy
+{
+public:
+    double p_bar;
+    int n;
+    double S0;
+    double K;
+    double T;
+public:
+    LeisenReimer(double sigma, double r, double dt, double _S0, double _K, double _T, int _n);
+    virtual ~LeisenReimer();
 };
 
 #endif // BINOMIALSTRATEGY_H
