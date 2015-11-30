d5b39f970cb6f16556beb7809500ff3c4729c810
mirceas08 <mircea.simionica@gmail.com>
2015-11-30 21:35:32 +0100

    some modifications

--- src/arithmeticAverage-fsg.cpp
+++ src/arithmeticAverage-fsg.cpp
@@ -14,9 +14,16 @@ using namespace arma;
 
 int main(int argc, char** argv)
 {
+    wall_clock timer;
+    timer.tic();
+
     std::string dataFile = "data/data.dat";
 
     cout << "Option price: " << forwardShootingGrid(dataFile) << std::endl;
+
+    double timeElapsed = timer.toc();
+    cout << "Time elapsed: " << timeElapsed << std::endl;
+
     return 0;
 }
 
